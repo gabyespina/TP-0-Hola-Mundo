2bc68857eff8e5d48b3a6951082c55dd6626085b
gabyespina <gabyespina.ge@yahoo.com>
2015-04-09 03:14:16 -0300

    hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/* El programa muestra en consola "Hola Mundo :)"
+* Gabriel Espina - K1091
+* 09/04/15
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hola Mundo :)";
+    return 0;
+}
